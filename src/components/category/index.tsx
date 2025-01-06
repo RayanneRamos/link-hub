36e529d4ca42797d6b7fa0fe549e82d9ca5fb93d
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-01-06 09:57:37 -0300

    utilizando propriedades no componente

--- src/components/category/index.tsx
+++ src/components/category/index.tsx
@@ -1,13 +1,18 @@
 import { colors } from "@/src/styles/colors";
 import { MaterialIcons } from "@expo/vector-icons";
-import { Pressable, Text } from "react-native";
+import { Pressable, PressableProps, Text } from "react-native";
 import { styles } from "./styles";
 
-export function Category() {
+interface CategoryProps extends PressableProps {
+  name: string;
+  icon: keyof typeof MaterialIcons.glyphMap;
+}
+
+export function Category({ name, icon, ...props }: CategoryProps) {
   return (
-    <Pressable style={styles.container}>
-      <MaterialIcons name="code" size={16} color={colors.gray[400]} />
-      <Text style={styles.name}>Projetos</Text>
+    <Pressable style={styles.container} {...props}>
+      <MaterialIcons name={icon} size={16} color={colors.gray[400]} />
+      <Text style={styles.name}>{name}</Text>
     </Pressable>
   );
 }
