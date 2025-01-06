f70b443bd415299e8f2c5e1d38b440a2e11bfa64
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-01-06 11:03:10 -0300

    utilizando modal

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -5,6 +5,7 @@ import {
   Image,
   TouchableOpacity,
   FlatList,
+  Modal,
 } from "react-native";
 import { styles } from "./styles";
 import { MaterialIcons } from "@expo/vector-icons";
@@ -37,6 +38,24 @@ export default function Index() {
         contentContainerStyle={styles.linksContent}
         showsVerticalScrollIndicator={false}
       />
+      <Modal transparent visible>
+        <View style={styles.modal}>
+          <View style={styles.modalContent}>
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalCategory}>Curso</Text>
+              <TouchableOpacity>
+                <MaterialIcons
+                  name="close"
+                  size={20}
+                  color={colors.gray[400]}
+                />
+              </TouchableOpacity>
+            </View>
+            <Text style={styles.modalLinkName}>Rocketseat</Text>
+            <Text style={styles.modalUrl}>https://www.rocketseat.com.br</Text>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
