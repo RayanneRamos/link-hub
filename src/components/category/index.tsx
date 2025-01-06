bfab60a181c6de1b8ff36e3902c9d75bb7319ab6
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-01-06 10:02:09 -0300

    aplicando variação de estilos

--- src/components/category/index.tsx
+++ src/components/category/index.tsx
@@ -6,13 +6,16 @@ import { styles } from "./styles";
 interface CategoryProps extends PressableProps {
   name: string;
   icon: keyof typeof MaterialIcons.glyphMap;
+  isSelected: boolean;
 }
 
-export function Category({ name, icon, ...props }: CategoryProps) {
+export function Category({ name, icon, isSelected, ...props }: CategoryProps) {
+  const color = isSelected ? colors.green[300] : colors.gray[400];
+
   return (
     <Pressable style={styles.container} {...props}>
-      <MaterialIcons name={icon} size={16} color={colors.gray[400]} />
-      <Text style={styles.name}>{name}</Text>
+      <MaterialIcons name={icon} size={16} color={color} />
+      <Text style={[styles.name, { color }]}>{name}</Text>
     </Pressable>
   );
 }
