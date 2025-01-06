746632751bd96331b4d13eb3ea1c927cc922dd7c
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-01-06 10:28:08 -0300

    lista de categorias

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -2,7 +2,7 @@ import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 import { MaterialIcons } from "@expo/vector-icons";
 import { colors } from "@/src/styles/colors";
-import { Category } from "@/src/components/category";
+import { Categories } from "@/src/components/categories";
 
 export default function Index() {
   return (
@@ -13,9 +13,7 @@ export default function Index() {
           <MaterialIcons name="add" size={32} color={colors.green[300]} />
         </TouchableOpacity>
       </View>
-      <Category name="Projeto" icon="code" isSelected />
-      <Category name="Site" icon="language" isSelected={false} />
-      <Category name="Vídeo" icon="movie" isSelected={false} />
+      <Categories />
     </View>
   );
 }
