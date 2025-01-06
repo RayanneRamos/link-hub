130f082b98addb7f458d7f7ac95a924867c6f215
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-01-06 10:44:08 -0300

    lista de links

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,4 +1,11 @@
-import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
 import { styles } from "./styles";
 import { MaterialIcons } from "@expo/vector-icons";
 import { colors } from "@/src/styles/colors";
@@ -15,10 +22,20 @@ export default function Index() {
         </TouchableOpacity>
       </View>
       <Categories />
-      <Link
-        name="Rocketseat"
-        url="https://www.rocketseat.com.br"
-        onDetails={() => console.log("clicou")}
+
+      <FlatList
+        data={["1", "2", "3"]}
+        keyExtractor={(item) => item}
+        renderItem={() => (
+          <Link
+            name="Rocketseat"
+            url="https://www.rocketseat.com.br"
+            onDetails={() => console.log("clicou")}
+          />
+        )}
+        style={styles.links}
+        contentContainerStyle={styles.linksContent}
+        showsVerticalScrollIndicator={false}
       />
     </View>
   );
