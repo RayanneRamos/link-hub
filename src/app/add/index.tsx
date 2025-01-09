478e5b8ca7f012dd6d94b282ed9021ae8713c127
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-01-09 10:14:22 -0300

    redirecionando o usuário

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -34,6 +34,10 @@ export default function Add() {
         url,
         category,
       });
+
+      Alert.alert("Sucesso", "Novo link foi adicionado.", [
+        { text: "Ok", onPress: () => router.back() },
+      ]);
     } catch (error) {
       Alert.alert("Erro", "Não foi possível salvar o link!");
       console.log(error);
